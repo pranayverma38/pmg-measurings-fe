4b8f477135d9d5875582d609401a16a9966a9f02
pranayverma38 <pranayverma38@gmail.com>
2026-06-21 16:12:41 +0530

    Add HomePage, HomeLayout, HeaderHome, and FooterHome components

---
+++ app/home/page.tsx
@@ -0,0 +1,10 @@
+import HomeLayout from "@/components/layout/HomeLayout";
+import Section1 from "@/components/sections/index-6/Section1";
+
+export default function HomePage() {
+    return (
+        <HomeLayout>
+            <Section1 />
+        </HomeLayout>
+    );
+}
---
+++ components/layout/HomeLayout.tsx
@@ -0,0 +1,32 @@
+import { MobileMenuCloneProvider } from "@/components/layout/MobileMenuCloneContext";
+import BackToTop from "@/components/elements/BackToTopServer";
+import ParallaxSceneEffect from "@/components/effects/ParallaxSceneEffect";
+import FooterHome from "@/components/layout/footer/FooterHome";
+import HeaderHome from "@/components/layout/header/HeaderHome";
+import ServerEffects from "@/components/layout/ServerEffects";
+
+interface HomeLayoutProps {
+    children?: React.ReactNode;
+    mainClass?: string;
+}
+
+export default function HomeLayout({
+    children,
+    mainClass = "bg-neutral-50",
+}: HomeLayoutProps) {
+    return (
+        <MobileMenuCloneProvider>
+            <ServerEffects />
+            <div id="top" />
+            <HeaderHome />
+            <div id="smooth-wrapper">
+                <div id="smooth-content" className="z-index-3">
+                    <main className={mainClass}>{children}</main>
+                    <FooterHome />
+                </div>
+            </div>
+            <ParallaxSceneEffect />
+            <BackToTop />
+        </MobileMenuCloneProvider>
+    );
+}
